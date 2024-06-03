d3e02036966685704d1d6acb29767c0968b5e8cf
Yanwen Xu <your_email@abc.example>
2024-06-04 00:02:37 +0100

    working

--- refresh.template.py
+++ refresh.template.py
@@ -1242,9 +1242,6 @@ def _convert_compile_commands(aquery_output):
             if file == 'external/bazel_tools/src/tools/launcher/dummy.cc':
                 continue  # Suppress Bazel internal files leaking through. Hopefully will prevent issues like https://github.com/hedronvision/bazel-compile-commands-extractor/issues/77
 
-            
-
-
             print("----------")
             print(file)
             print("----------")
@@ -1293,7 +1290,8 @@ def _get_commands(target: str, flags: str):
         # Aquery docs if you need em: https://docs.bazel.build/versions/master/aquery.html
         # Aquery output proto reference: https://github.com/bazelbuild/bazel/blob/master/src/main/protobuf/analysis_v2.proto
         # One bummer, not described in the docs, is that aquery filters over *all* actions for a given target, rather than just those that would be run by a build to produce a given output. This mostly isn't a problem, but can sometimes surface extra, unnecessary, misconfigured actions. Chris has emailed the authors to discuss and filed an issue so anyone reading this could track it: https://github.com/bazelbuild/bazel/issues/14156.
-        f"mnemonic('(Objc|Cpp|Cuda)Compile',{target_statment})",
+        f"mnemonic('FileWrite|AvCcCompile|CudaDeviceLink|LinkExec',{target_statment})",
+        # f"mnemonic('(Objc|Cpp|Cuda)Compile',{target_statment})",
         # We switched to jsonproto instead of proto because of https://github.com/bazelbuild/bazel/issues/13404. We could change back when fixed--reverting most of the commit that added this line and tweaking the build file to depend on the target in that issue. That said, it's kinda nice to be free of the dependency, unless (OPTIMNOTE) jsonproto becomes a performance bottleneck compated to binary protos.
         '--output=jsonproto',
         # We'll disable artifact output for efficiency, since it's large and we don't use them. Small win timewise, but dramatically less json output from aquery.
